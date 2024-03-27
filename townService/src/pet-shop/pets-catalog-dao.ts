8f51bbfa1f0b6ebc573dbded11c47bb9f70effce
Ananya Radhakrishnan <ananyar807@gmail.com>
2024-03-27 15:19:48 -0400

    removed try catch in dao

--- townService/src/pet-shop/pets-catalog-dao.ts
+++ townService/src/pet-shop/pets-catalog-dao.ts
@@ -8,10 +8,5 @@ export const findPetByType = (type: string) => petsCatalogModel.findOne({ type }
 
 // Find the pet catalog entry with the given type and update its counter by 1
 export const updateCounterForPet = async (type: string) => {
-  try {
-    await petsCatalogModel.findOneAndUpdate({ type }, { $inc: { counter: 1 } }, { new: true });
-  } catch (error) {
-    console.error('Error updating counter for pet:', error);
-    throw error;
-  }
+  await petsCatalogModel.findOneAndUpdate({ type }, { $inc: { counter: 1 } }, { new: true });
 };
