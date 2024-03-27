5583161266ce1e472497158b99b60531d63ed896
Ananya Radhakrishnan <ananyar807@gmail.com>
2024-03-27 12:07:25 -0400

    attempt to fix build x2

--- townService/src/pet-shop/pets-catalog-model.ts
+++ townService/src/pet-shop/pets-catalog-model.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import petsCatalogSchema from './pets-catalog-schema.js';
+import petsCatalogSchema from './pets-catalog-schema';
 
 const petsCatalogModel = mongoose.model('pet-shop', petsCatalogSchema);
 export default petsCatalogModel;
