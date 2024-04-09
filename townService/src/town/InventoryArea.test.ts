1656a7eebe6a0a7bb734b01c2a86f2313df2f805
akannalath <kannalath.a@northeastern.edu>
2024-04-09 11:51:17 -0400

    Added PR Suggestions

--- townService/src/town/InventoryArea.test.ts
+++ townService/src/town/InventoryArea.test.ts
@@ -97,7 +97,6 @@ describe('InventoryArea', () => {
       );
       expect(val.boundingBox).toEqual({ x, y, width, height });
       expect(val.id).toEqual(name);
-      expect(val.pets)?.toBeUndefined();
       expect(val.occupantsByID).toEqual([]);
     });
   });
