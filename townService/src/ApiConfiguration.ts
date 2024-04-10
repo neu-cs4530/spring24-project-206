cb0cceb6a4c689ed783cb014fbfeffc34f40d056
Sanjana Kashyap <kashyap.san@northeastern.edu>
2024-04-10 10:30:23 -0400

    change link

--- townService/src/ApiConfiguration.ts
+++ townService/src/ApiConfiguration.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 import 'dotenv/config';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_AXIOS_BASE_URL ?? 'http://localhost:8081/api',
+  baseURL:
+    process.env.NEXT_PUBLIC_AXIOS_BASE_URL ??
+    'https://covey-town-deployment-28400a9c8dfd.herokuapp.com/api',
 });
 
 export default instance;
